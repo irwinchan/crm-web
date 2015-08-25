71c4e34b415bdb0d8d94a56727702e2775732296
Irwin <irwinchan@me.com>
2015-08-25 14:47:33 -0400

    Add placeholder routes and views for other menu items.

--- crm.rb
+++ crm.rb
@@ -9,3 +9,25 @@ end
 get '/contacts' do
   erb :contacts
 end
+
+get '/add_contact' do
+  erb :add_contact
+end
+
+get '/modify_contact' do
+  erb :modify_contact
+end
+
+get '/display_contact' do
+  erb :display_contact
+end
+
+get '/display_contact_attribute' do
+  erb :display_contact_attribute
+end
+
+get '/delete_contact' do
+  erb :delete_contact
+end
+
+
