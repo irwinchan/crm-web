24b1da088213fd5b620085a34aa5bb61c560a734
Irwin <irwinchan@me.com>
2015-08-26 15:09:03 -0400

    Modify routes and layout to display the proper title

--- crm.rb
+++ crm.rb
@@ -6,14 +6,17 @@ $rolodex = Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"
+  @title = "My CRM - Main Menu"
   erb :index
 end
 
 get '/contacts' do
+  @title = "My CRM - All Contacts"
   erb :contacts
 end
 
 get '/contacts/new' do
+  @title = "My CRM - Create New Contact"
   erb :new_contact
 end
 
@@ -22,18 +25,22 @@ end
 # end
 
 get '/modify_contact' do
+  @title = "My CRM - Modify Contact"
   erb :modify_contact
 end
 
 get '/display_contact' do
+  @title = "My CRM - Display Contact"
   erb :display_contact
 end
 
 get '/display_contact_attribute' do
+  @title = "My CRM - Display Contact Attribute"
   erb :display_contact_attribute
 end
 
 get '/delete_contact' do
+  @title = "My CRM - Delete Contact"
   erb :delete_contact
 end
 
