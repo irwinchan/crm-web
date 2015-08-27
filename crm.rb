6e9da23bfe585e045868981bc95b3b4bf021855f
Irwin <irwinchan@me.com>
2015-08-26 20:21:13 -0400

    Create two column layout, and added styling

--- crm.rb
+++ crm.rb
@@ -7,16 +7,19 @@ $rolodex = Rolodex.new
 get '/' do
   @crm_app_name = "My CRM"
   @title = "My CRM - Main Menu"
+  @banner_title = "Main Menu"
   erb :index
 end
 
 get '/contacts' do
   @title = "My CRM - All Contacts"
+   @banner_title = "All Contacts"
   erb :contacts
 end
 
 get '/contacts/new' do
   @title = "My CRM - Create New Contact"
+  @banner_title = "Create New Contact"
   erb :new_contact
 end
 
@@ -26,21 +29,25 @@ end
 
 get '/modify_contact' do
   @title = "My CRM - Modify Contact"
+   @banner_title = "Modify Contact"
   erb :modify_contact
 end
 
 get '/display_contact' do
   @title = "My CRM - Display Contact"
+   @banner_title = "Display Contact"
   erb :display_contact
 end
 
 get '/display_contact_attribute' do
   @title = "My CRM - Display Contact Attribute"
+   @banner_title = "Display Contact Attribute"
   erb :display_contact_attribute
 end
 
 get '/delete_contact' do
   @title = "My CRM - Delete Contact"
+   @banner_title = "Delete Contact"
   erb :delete_contact
 end
 
